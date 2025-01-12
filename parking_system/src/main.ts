ffa968a5e348666b47f1e41c826e0037bc494ec9
hamza23177 <hamzamhamd23177@gmail.com>
2025-01-12 01:34:38 -0800

    resolve error

--- parking_system/src/main.ts
+++ parking_system/src/main.ts
@@ -3,40 +3,43 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as os from 'os';
 import * as helmet from 'helmet';
-// eslint-disable-next-line @typescript-eslint/no-require-imports
 const cluster = require('node:cluster');
 
 const numCPUs = os.cpus().length;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  // إعداد CSP لمنع XSS
-  app.use(
-    helmet.contentSecurityPolicy({
-      directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: ["'self'"],
-        objectSrc: ["'none'"],
-        upgradeInsecureRequests: [],
-      },
-    }),
-  );
-
-
-  cluster.schedulingPolicy = cluster.SCHED_RR;
-
   if (cluster.isPrimary) {
-    for (let i = 0; i < numCPUs - numCPUs + 1; i++) {
+    // إذا كانت هذه العملية هي العملية الرئيسية، نقوم بإنشاء عمليات ثانوية
+    for (let i = 0; i < numCPUs; i++) {
       cluster.fork();
     }
-    console.log(`----------------------------------------`);
-    console.log(`Welcome`);
-    console.log(`----------------------------------------`);
+    console.log('----------------------------------------');
+    console.log('Welcome');
+    console.log('----------------------------------------');
+    
+    // يمكنك إضافة حدث للإبلاغ عند انتهاء أي من العمليات الثانوية
+    cluster.on('exit', (worker, code, signal) => {
+      console.log(`Worker ${worker.process.pid} died`);
+    });
+    
   } else {
+    // إذا كانت هذه عملية ثانوية، نقوم بإنشاء التطبيق والاستماع على المنفذ
     const app = await NestFactory.create(AppModule);
+
+    // إعداد CSP لمنع XSS
+    app.use(
+      helmet.contentSecurityPolicy({
+        directives: {
+          defaultSrc: ["'self'"],
+          scriptSrc: ["'self'"],
+          objectSrc: ["'none'"],
+          upgradeInsecureRequests: [],
+        },
+      }),
+    );
+
     await app.listen(process.env.PORT ?? 3000);
   }
-  await app.listen(3000);
 }
+
 bootstrap();
